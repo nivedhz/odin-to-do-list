8b590ef181ee993d84daa418217fb5cb3c093d43
Nivedh E R <nivedher12@gmail.com>
2026-04-30 22:52:32 +0530

    fix(createSidebar): removed unnecessary argument

--- src/ui/Sidebar.js
+++ src/ui/Sidebar.js
@@ -2,7 +2,7 @@ import "./sidebar.css";
 import { createProjectDetails } from "./ProjectDetails.js";
 import { createAccountDetails } from "./AccountDetails.js";
 
-function createSidebar(mainContainer) {
+function createSidebar() {
   const sidebarContainer = document.createElement("div");
   sidebarContainer.classList.add("sidebar-container");
   const account = createAccountDetails();

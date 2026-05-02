ae55c14daad3b47b63bf27ac23d5a81991bdfef6
Nivedh E R <nivedher12@gmail.com>
2026-05-02 20:45:32 +0530

    feat(userProfileForm): added the modal and styled it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import "./styles/index.css";
 import { createSidebar } from "./ui/Sidebar.js";
 import { createMainTodoSection } from "./ui/MainTodoSection.js";
+import { createProfileFormModal } from "./ui/ProfileFormModal.js";
 
 const DOM = {
   container: {
     main: document.querySelector("#container"),
+    body: document.querySelector("body"),
   },
 };
 
 const todoSection = createMainTodoSection();
 const sidebar = createSidebar();
+const userModal = createProfileFormModal();
 
 DOM.container.main.append(sidebar, todoSection);
+DOM.container.body.prepend(userModal);
---
+++ src/ui/ProfileFormModal.js
@@ -0,0 +1,43 @@
+import defaultUserProfile from "../assets/images/default-user.svg";
+import "../styles/profile-modal-form.css";
+
+function createProfileFormModal() {
+  const modalContainer = document.createElement("div");
+  modalContainer.classList.add("profile-modal__container", "hidden");
+  const formContainer = document.createElement("form");
+  formContainer.method = "POST";
+  formContainer.classList.add("profile-modal__form-container");
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("profile-modal__img-container");
+  const imgPreview = document.createElement("img");
+  imgPreview.id = "profile-modal__img-preview";
+  imgPreview.src = defaultUserProfile;
+  const imgInput = document.createElement("input");
+  imgInput.setAttribute("name", "profilePicture");
+  imgInput.classList.add("profile-modal__img-input");
+  imgInput.type = "file";
+  imgInput.accept = "image/*";
+
+  const usernameContainer = document.createElement("div");
+  usernameContainer.classList.add("profile-modal__username-container");
+  const usernameLabel = document.createElement("label");
+  usernameLabel.setAttribute("for", "profile-modal__username");
+  const usernameInput = document.createElement("input");
+  usernameInput.setAttribute("name", "profileUsername");
+  usernameInput.id = "profile-modal__username";
+  usernameInput.classList.add("profile-modal__username-input");
+  usernameLabel.textContent = "Username: ";
+
+  const profileSubmitBtn = document.createElement("button");
+  profileSubmitBtn.classList.add("profile-modal__username-btn");
+  profileSubmitBtn.type = "submit";
+  profileSubmitBtn.textContent = "Submit";
+
+  imgContainer.append(imgPreview, imgInput);
+  usernameContainer.append(usernameLabel, usernameInput);
+  formContainer.append(imgContainer, usernameContainer, profileSubmitBtn);
+  modalContainer.append(formContainer);
+  return modalContainer;
+}
+
+export { createProfileFormModal };

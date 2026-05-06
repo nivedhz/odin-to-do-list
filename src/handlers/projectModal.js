22d35498beeb1efdc1e0ab259bc56c21d487750e
Nivedh E R <nivedher12@gmail.com>
2026-05-06 11:52:49 +0530

    feat(projectModal queries): added the queries in project modal

--- src/handlers/projectModal.js
+++ src/handlers/projectModal.js
@@ -1,32 +1,24 @@
 import { createProject } from "../data/state.js";
-import { reloadProjects } from "../ui/render.js";
+import { renderProjects } from "../ui/render.js";
 
-function toggleHidden() {
+export function projectModalToggleHidden() {
   document
     .querySelector(".project-modal__container")
     .classList.toggle("hidden");
 }
+
 function initProjectModal() {
-  document
-    .querySelector(".project__add-btn")
-    .addEventListener("click", toggleHidden);
-  document
-    .querySelector(".project-modal__quit-btn")
-    .addEventListener("click", toggleHidden);
   document
     .querySelector(".project-modal__form-container")
     .addEventListener("submit", (event) => {
       event.preventDefault();
-      toggleHidden();
+      projectModalToggleHidden();
       const data = new FormData(
         document.querySelector(".project-modal__form-container"),
       );
       const values = Object.fromEntries(data.entries());
       createProject(values.projectName);
-      reloadProjects();
-      document
-        .querySelector(".project-name__edit-btn")
-        .addEventListener("click", toggleHidden);
+      renderProjects();
       document.querySelector(".project-modal__form-container").reset();
     });
 }
--- src/ui/render.js
+++ src/ui/render.js
@@ -7,12 +7,20 @@ import {
 } from "./modals/modal.js";
 import { createProjectDetailName } from "../ui/sidebar/ProjectDetails.js";
 import { state } from "../data/state.js";
+import { projectModalToggleHidden } from "../handlers/projectModal.js";
+
+function initProjectQuery() {
+  document
+    .querySelector(".project__add-btn")
+    .addEventListener("click", projectModalToggleHidden);
+}
 
 function renderMainContent(container) {
   const sidebar = createSidebar();
   const todoSection = createMainTodoSection();
 
   container.append(sidebar, todoSection);
+  initProjectQuery();
 }
 
 function renderModals(root) {
@@ -21,15 +29,22 @@ function renderModals(root) {
   const projectModal = createProjectFormModal();
 
   root.prepend(userModal, taskModal, projectModal);
+  document
+    .querySelector(".project-modal__quit-btn")
+    .addEventListener("click", projectModalToggleHidden);
 }
 
-function reloadProjects() {
+function renderProjects() {
   document.querySelector(".project__bottom-container").replaceChildren();
   state.projects.forEach((project) => {
     document
       .querySelector(".project__bottom-container")
       .append(createProjectDetailName(project.projectName));
   });
+  console.log(document.querySelectorAll(".project__edit-btn"));
+  document.querySelectorAll(".project__edit-btn").forEach((editBtn) => {
+    editBtn.addEventListener("click", projectModalToggleHidden);
+  });
 }
 
-export { renderMainContent, renderModals, reloadProjects };
+export { renderMainContent, renderModals, renderProjects };
